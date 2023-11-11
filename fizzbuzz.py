927447e378fdc92c36154d3b3469f88d5d202680
Chris Lambert <chrslmbrt@gmail.com>
2023-11-11 18:16:44 -0500

    sorry for the confusion

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,12 @@
 # add your code here
+#attempt 2 at fizzbuzz challenge.  My first attempt failed.  I had a couple of issues in my first iteration.
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
 
